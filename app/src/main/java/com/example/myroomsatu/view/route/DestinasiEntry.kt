e57c496943eeeebd645fdcbad3455411a6d186b2
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-28 19:11:35 +0700

    Tambahkan DestinasiEntry untuk navigasi

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R
+
+object DestinasiEntry : DestinasiNavigasi(
+    route = "item_entry",
+    titleRes = R.string.entry_siswa
+)
+
